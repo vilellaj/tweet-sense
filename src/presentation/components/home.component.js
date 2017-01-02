b48e7e2d0bbc7a58e1a5f2e765866c426ec57a9e
vilellaj <vilellaj@hotmail.com>
2017-01-02 14:47:45 -0500

    request and layout test

--- src/presentation/components/home.component.js
+++ src/presentation/components/home.component.js
@@ -1,13 +1,49 @@
 'use strict';
 
 import React from 'react';
+import axios from 'axios';
 
 export default class HomeComponent extends React.Component {
+  
+  constructor(props)
+  {
+    super(props);
+    
+    this.state = {query: ''};
+    
+    this.propTypes = {
+        clickEvent: React.PropTypes.func
+    };
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.findResults = this.findResults.bind(this);
+  }
+  
+  handleChange(event) {
+    this.setState({query: event.target.value});
+  }
+  
+  findResults(e)
+  {
+    axios.get('/search', {
+      params: {
+        query: '#12345'
+      }
+    })
+    .then(function (response) {
+      alert(response);
+    })
+    .catch(function (error) {
+      alert(error);
+    });    
+  }
+    
   render() {
     return (
       <div className="home">
         <div className="home-content">
-          <input type="text"/>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <button onClick={this.findResults}>Search</button>
         </div>
       </div>
     );
--- src/presentation/components/layout.component.js
+++ src/presentation/components/layout.component.js
@@ -3,23 +3,40 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+
 export default class LayoutComponent extends React.Component {
+  
+  constructor(props)
+  {
+    super(props);      
+  }
   render() {
     return (
       <div className="app-container">
-        <header>
-          <Link to="/">
-            Home
-          </Link>
-          <Link to="/test">
-            Test
-          </Link>
-        </header>
-        <div className="app-content">{this.props.children}</div>
-        <footer>
-          <p>
-            This is a demo app to showcase universal rendering and routing with <strong>React</strong> and <strong>Express</strong>.
-          </p>
+        <header id="header" className="clearfix" data-current-skin="white">
+            <div className="divHeader">
+                <ul className="header-inner">
+                    <li className="logo hidden-xs">
+                        <img src="http://niche-macworld-production.s3.amazonaws.com/wp-content/uploads/2010/08/Tweet-button.jpg" alt="logo-tweet-sense" height="30px;"/> 
+                        <span>Tweet-Sense</span>
+                    </li>            
+                    <li className="logo pull-right">
+                      <Link to="/">
+                        Tweet-sense
+                      </Link>
+                    </li>
+                </ul>
+            </div>
+        </header> 
+        <section id="main" data-layout="layout-1">
+            <section id="content">
+                <div className="container">
+                    <div className="app-content">{this.props.children}</div>
+                </div>
+            </section>
+        </section>               
+        <footer>          
+            This is a demo app to showcase universal rendering and routing with <strong>React</strong> and <strong>Express</strong>.          
         </footer>
       </div>
     );
